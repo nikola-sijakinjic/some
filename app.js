71a1b006cbaf11d95281415f9d310dc6cec045c9
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-07-13 14:39:48 +0200

    hello world init works on 3000

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-express = require('express');
+const express = require('express')
+const app = express()
 
-var seze = "";
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
