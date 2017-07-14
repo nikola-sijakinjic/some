808adc05ddcd32a5a20ade3d4cd97269be057669
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-07-14 12:24:11 +0200

    e2e broken

--- e2e/health.test.js
+++ e2e/health.test.js
@@ -3,7 +3,7 @@ var expect = chakram.expect;
 
 describe("HTTP assertions", function () {
   it("should make HTTP assertions easy", function () {
-    var response = chakram.get("http://localhost:3000/");
+    var response = chakram.get("http://localhost:32000/");
     expect(response).to.have.status(200);
     return chakram.wait();
   });
