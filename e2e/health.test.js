eefd26331ec0891dffc495062bc361dbe13de9cf
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-07-14 12:16:43 +0200

    e2e passed

--- e2e/health.test.js
+++ e2e/health.test.js
@@ -1,3 +1,6 @@
+var chakram = require('chakram');
+var expect = chakram.expect;
+
 describe("HTTP assertions", function () {
   it("should make HTTP assertions easy", function () {
     var response = chakram.get("http://localhost:3000/");
